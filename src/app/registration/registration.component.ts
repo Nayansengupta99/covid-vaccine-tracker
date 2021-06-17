e4e487bb2f3649b3262e7364042afff80fb04df9
Nayansengupta99 <nayansengupta99@gmail.com>
2021-06-17 10:49:25 +0530

    form submission value fixed

--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -12,6 +12,7 @@ import * as moment from 'moment';
 export class RegistrationComponent implements OnInit {
 
   success = false;
+  districtArray = []
   payload: any;
   payloadString: any;
   msg: any
@@ -25,6 +26,7 @@ export class RegistrationComponent implements OnInit {
   state: Array<any> = []
   message: any;
   vC: string = ""
+  districtName: string = ""
   district: string = "--Choose District--";
 
   constructor(private service: UserRegistrationService, private datePipe: DatePipe) { }
@@ -56,7 +58,8 @@ export class RegistrationComponent implements OnInit {
     this.service.getDistricts().subscribe(res => {
 
       this.response = res;
-      console.log(res)
+      this.districtArray = this.response.district
+      console.log()
 
     });
 
@@ -68,8 +71,15 @@ export class RegistrationComponent implements OnInit {
 
 
   displaySelectedDistrict(event: any) {
+
     console.log(event.target.value);
     this.id = event.target.value
+    for (let i = 0; i < this.response.districts.length; i++) {
+      if (this.response.districts[i].district_id == this.id) {
+        this.districtName = this.response.districts[i].district_name
+      }
+
+    }
     this.date = new Date();
     let latest_date = this.datePipe.transform(this.date, 'dd-MM-yyyy');
     console.log(this.date)
@@ -81,12 +91,12 @@ export class RegistrationComponent implements OnInit {
     });
   }
   public registerNow() {
-    this.user.district = this.district
+    this.user.district = this.districtName
 
     this.user.vaccineCenter = this.vC
 
     this.user.age = this.age
-    console.log( this.user.district)
+    console.log(this.user.district)
     console.log(this.user.vaccineCenter)
 
     let resp = this.service.doRegister(this.user);
